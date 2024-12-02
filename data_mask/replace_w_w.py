99d690f79fcc316175a5f2bf2f0479930581aa7a
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2024-12-02 18:00:39 +0200

    Converter mask

---
+++ data_mask/replace_w_w.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import numpy as np
+import os
+
+imgs = os.listdir()
+
+for i,im in enumerate(imgs):
+	if ".bmp" in im:
+		img = Image.open(im)
+		img = np.array(img)
+		mask = np.any(img != [0,0,0], axis=-1)
+		img[mask] = [255,255,255]
+		processed_image = Image.fromarray(img)
+		processed_image.save(im.replace("_mask",""))
