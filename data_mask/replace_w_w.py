b85bddd61d7304a2401cc0f5c1aea8f9496c9ef6
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2024-12-07 00:58:09 +0200

    modify som 4 sec work script cuz i ve been so lazy

--- data_mask/replace_w_w.py
+++ data_mask/replace_w_w.py
@@ -5,10 +5,11 @@ import os
 imgs = os.listdir()
 
 for i,im in enumerate(imgs):
-	if ".bmp" in im:
+	if "_mask.bmp" in im:
 		img = Image.open(im)
 		img = np.array(img)
 		mask = np.any(img != [0,0,0], axis=-1)
 		img[mask] = [255,255,255]
 		processed_image = Image.fromarray(img)
 		processed_image.save(im.replace("_mask",""))
+		os.remove(im)
